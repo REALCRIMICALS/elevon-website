5af6fb45ccdf6db287e51b3ce7212d2c3feb08fc
DaBluLite <dablulite@gmail.com>
2025-05-26 22:02:38 +0300

    Emails fix

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,7 +1,8 @@
 'use server'
 
 import { Resend } from 'resend';
-import { Html, Tailwind, Text, Container, Body } from '@react-email/components';
+import MessageReceivedEmail from '@/emails/message-received';
+import HasSentMessageEmail from '@/emails/has-sent-message';
 
 export async function sendMessage(previousState: { status: string }, formData: FormData) {
     const name = formData.get('name');
@@ -15,21 +16,7 @@ export async function sendMessage(previousState: { status: string }, formData: F
             from: 'Elevon Studios <noreply@elevon.gg>',
             to: [email as string],
             subject: 'We got your message!',
-            react: (() => {
-                return <Tailwind>
-                    <Html>
-                        <Body className='m-0 antialiased bg-blue-950'>
-                            <Container className='p-4 w-full bg-gray-500/5'>
-                                <Text className='text-3xl w-full text-center'>Elevon</Text>
-                            </Container>
-                            <Container className='p-4 w-full flex flex-col gap-4'>
-                                <Text className='text-xl w-full text-center'>{name as string}, thank you for your message</Text>
-                                <Text className='text-xl w-full text-center'>We will try to get in contact with you as soon as possible.</Text>
-                            </Container>
-                        </Body>
-                    </Html>
-                </Tailwind>
-            })(),
+            react: MessageReceivedEmail({ name: name as string }),
         })
     } catch (e) {
         return { status: "error", errorMsg: e }
@@ -40,21 +27,7 @@ export async function sendMessage(previousState: { status: string }, formData: F
             from: 'Elevon Studios <noreply@elevon.gg>',
             to: ["contact@elevon.gg"],
             subject: 'Message from user',
-            react: (() => {
-                return <Tailwind>
-                    <Html>
-                        <Body className='m-0 antialiased bg-blue-950'>
-                            <Container className='p-4 w-full bg-gray-500/5'>
-                                <Text className='text-3xl w-full text-center'>Elevon</Text>
-                            </Container>
-                            <Container className='p-4 w-full flex flex-col gap-4'>
-                                <Text className='text-xl w-full'>{name as string} ({email as string}) has sent a message</Text>
-                                <Text className='text-xl w-full bg-gray-500/5 rounded-md'>{message as string}</Text>
-                            </Container>
-                        </Body>
-                    </Html>
-                </Tailwind>
-            })(),
+            react: HasSentMessageEmail({ email: email as string, name: name as string, message: message as string }),
         })
     } catch (e) {
         return { status: "error", errorMsg: e }
---
+++ src/emails/has-sent-message.tsx
@@ -0,0 +1,25 @@
+import { Tailwind, Body, Container, Img, Html, Text, Column, Row, Section } from "@react-email/components";
+
+export default function HasSentMessageEmail({ name, email, message }: { name: string, email: string, message: string }) {
+    return <Tailwind>
+        <Html>
+            <Body className='m-0 antialiased bg-blue-950 text-white [font-family:_Trebuchet_MS,Helvetica,sans-serif]'>
+                <Section className='w-[calc(100%-32px)] border border-[#ffffff0c] border-solid rounded-xl m-4 bg-gray-500/10 items-center flex justify-center gap-2 p-3 select-none'>
+                    <Row>
+                        <Column>
+                            <Img width={36} height={36} src={"https://www.elevon.gg/elevon.png"} />
+                        </Column>
+                        <Column className="w-2"/>
+                        <Column>
+                            <Text className='text-3xl text-center m-0'>Elevon</Text>
+                        </Column>
+                    </Row>
+                </Section>
+                <Container className='p-4 w-full flex flex-col gap-4'>
+                    <Text className='text-xl w-full'>{name as string} ({email as string}) has sent a message</Text>
+                    <Text className='w-full bg-gray-500/5 rounded-md border border-white/6'>{message as string}</Text>
+                </Container>
+            </Body>
+        </Html>
+    </Tailwind>
+}
